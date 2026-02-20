5cc9b44e11ca6a0e56abb1e42c227a5d9b717a2c
Pillapalem Tejaswi <154959832+tejasw-123@users.noreply.github.com>
2026-02-20 18:51:00 +0530

    Add files via upload

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../../assets/net1.png'
+const  Navbar = () => {
+  return (
+    <div className='navbar'>
+      <div className='navbar-left'>
+        <img src={logo} alt="" />
+        <ul>
+          <li>Home</li>
+          <li>TV Shows</li>
+          <li>Movies</li>
+          <li>New & Popular</li>
+          <li>My List</li>
+          <li>Browse by Language</li>
+        </ul>
+      </div>
+      <div className='navbar-right'></div>
+
+    </div>
+  )
+}
+
+export default  Navbar
